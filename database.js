77842f0ad8f6a7ffdc1a26521a4e963a8268d125
Jared Rogers <rogersjaredt@gmail.com>
2021-05-15 18:21:28 -0700

    removed unneccessary pool.connect

--- database.js
+++ database.js
@@ -3,14 +3,6 @@ const { config } = require('./dbConfig.js');
 //---------------------------------------------------
 
 const pool = new Pool(config);
-pool.connect((err, success) => {
-  if (err) {
-    console.log('😢 error connecting to database 😞', err);
-  } else {
-  console.log('👍👍 Successfully connected to the database! 🔥🔥🔥');
-  }
-});
-
 
 const calculateQueryRange = (page, count) => {
   const queryRange = {
